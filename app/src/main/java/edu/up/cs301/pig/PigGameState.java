7c6d5cf459d7da5d97fd336ffa072e99238eb813
Aron <manalang23@up.edu>
2020-10-06 12:12:57 -1000

    1A

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -26,4 +26,44 @@ public class PigGameState extends GameState {
         instance.diceValue = pigGameState.diceValue;
         return instance;
     }
+
+    private int getTurn() {
+        return this.turn;
+    }
+
+    private int getP1Score() {
+        return this.p1Score;
+    }
+
+    private int getP2Score() {
+        return this.p2Score;
+    }
+
+    private int getRunningTotal() {
+        return this.runningTotal;
+    }
+
+    private int getDiceValue() {
+        return this.diceValue;
+    }
+
+    private void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    private void setP1Score(int p1Score) {
+        this.p1Score = p1Score;
+    }
+
+    private void setP2Score(int p2Score) {
+        this.p2Score = p2Score;
+    }
+
+    private void setRunningTotal(int runningTotal) {
+        this.runningTotal = runningTotal;
+    }
+
+    private void setDiceValue(int diceValue) {
+        this.diceValue = diceValue;
+    }
 }

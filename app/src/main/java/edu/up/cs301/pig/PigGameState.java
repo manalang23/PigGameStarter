97793525627cbdf860b7cad6e007db6633b7b53a
Aron <manalang23@up.edu>
2020-10-06 11:51:26 -1000

    Start of CP1

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -1,4 +1,21 @@
 package edu.up.cs301.pig;
 
-public class PigGameState {
+import edu.up.cs301.game.infoMsg.GameState;
+
+public class PigGameState extends GameState {
+    private int turn;
+    private int p1Score;
+    private int p2Score;
+    private int runningTotal;
+    private int diceValue;
+
+    public PigGameState() {
+        turn = 0;
+        p1Score = 0;
+        p2Score = 0;
+        runningTotal = 0;
+        diceValue = 0;
+    }
+
+    private PigGameState copy()
 }
